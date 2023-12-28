b73f77ac007a7425392c949cb53a84efdce3feb0
saikumar7362 <lakshminarasimha7362@gmail.com>
2023-12-28 19:49:27 +0530

    Operators

--- 01Sai_Operators.java
+++ 01Sai_Operators.java
@@ -6,6 +6,22 @@ package javaleaning;
         int c = 5;
         //arthmatic operators
         int result1 = (a+b)+(a*b)*(a/b)/(a-c)-(a%b);
-        System.out.println("Arthmat9ic operators :" +result1);
+        System.out.println("Arthmatic operators :" +result1);
+        //Assignment operators
+        int result2 = result1;
+            result2 += 5;
+        System.out.println("Assignment :" +result2); 
+        int result3 = result2;
+         result3 -= 10;
+        System.out.println("Assignment :" +result3 );
+        //logical Operators
+        boolean condition1 = (a>b);
+        boolean condition2 = (c!=5);
+        boolean result4 = condition1 || condition2;
+        System.out.println("logical operators :" +result4);
+        System.out.println(!(a>=3 && a< 11));
+        //relational operators
+        boolean result5 = (result1>= 30);
+        System.out.println(result5);
     }
 }
